ca1c3d0447f4f6b3cdf977313a7dc1990c0a60ae
BlindBatman <34074590+BlindBatman@users.noreply.github.com>
2018-07-31 11:34:27 -0600

    main testing

---
+++ Chess/MVC/Model/Board.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.MVC.Model
+{
+    class Board
+    {
+        private string[][] gameBoard = new string[8][8];
+
+        public void GenerateBoard()
+        {
+            for (int i = 0; i < gameBoard.Length; i++)
+            {
+                for (int j = 0; j <gameBoard[i].Length; j++)
+                {
+                    Console.WriteLine(gameBoard[i][j] = "-");
+                }
+            }
+        }
+    }
+}
--- Chess/Program.cs
+++ Chess/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Chess.MVC.Model;
 
 namespace Chess
 {
@@ -11,6 +12,8 @@ namespace Chess
         static void Main(string[] args)
         {
             //Chess Application
+            Board a = new Board();
+            a.GenerateBoard();
         }
     }
 }
